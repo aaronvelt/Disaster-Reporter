bbb97e508037e6c21326df00325a5a3f9a37a5fe
Aaron Veltman <31473798+aaronvelt@users.noreply.github.com>
2022-01-13 14:59:09 +0100

    ScrollingText

--- Assets/Scripts/Managers/TeleprompterManager.cs
+++ Assets/Scripts/Managers/TeleprompterManager.cs
@@ -1,16 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using ScriptableObjects;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TeleprompterManager : MonoBehaviour
 {
+    public static TeleprompterManager instance;
     [SerializeField]
     private Teleprompter[] teleprompters;
     [SerializeField]
     private SegmentScriptableObject[] segments;
-
     private int _index = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            NextSegment();
+        }
+    }
 
     public void NextSegment()
     {
--- Assets/Scripts/Teleprompter.cs
+++ Assets/Scripts/Teleprompter.cs
@@ -1,23 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using ScriptableObjects;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Teleprompter : MonoBehaviour
 {
+    [SerializeField] private RectTransform textBox;
+    [SerializeField] private Text textContent;
     [SerializeField] private string telepromterName;
-    public bool playerLooked = false;
-    
+    [SerializeField] private float textSpeed;
+    public bool playerLooked;
+    private float _height;
+    private bool _active;
+
     void Update()
     {
-        if (playerLooked)
+        if (_active)
         {
-            Debug.Log( Time.time + "| I see: " + telepromterName);
-            playerLooked = false;
+            ScrollSegment();
         }
     }
 
-    public void StartSegment(ScriptableObject segment)
+    public void StartSegment(SegmentScriptableObject segment)
     {
-        
+        textContent.text = segment.text;
+        _height = textContent.preferredHeight;
+        textBox.anchoredPosition = new Vector3(0, -(_height / 2 + 10), -20);
+        _active = true;
+    }
+    
+    private void ScrollSegment()
+    {
+        Vector3 position = textBox.anchoredPosition;
+        position = new Vector3(0, position.y + Time.deltaTime * textSpeed, position.z);
+        textBox.anchoredPosition = position;
+
+        if (textBox.anchoredPosition.y >= (_height / 2 + 5))
+        {
+            _active = false;
+            textContent.text = " ";
+            textBox.anchoredPosition = new Vector3(0, -10, -20);
+            TeleprompterManager.instance.NextSegment();
+        }
     }
 }
