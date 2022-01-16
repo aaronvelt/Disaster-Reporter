c7b1d73bca17ddc41803f7b734f2eda1ee239de0
Aaron Veltman <31473798+aaronvelt@users.noreply.github.com>
2022-01-16 21:10:52 +0100

    Studio with segments

--- Assets/Scripts/Teleprompter.cs
+++ Assets/Scripts/Teleprompter.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class Teleprompter : MonoBehaviour
 {
     public Text textContent;
+    public RectTransform textBox;
+    public MeshRenderer lightMesh;
+    public Material LightOffMaterial;
+    public Material LightOnMaterial;
     public float textSpeed;
-    [SerializeField] private RectTransform textBox;
-    [SerializeField] private float textOffSet;
+    public float textOffSet;
     [NonSerialized]public bool playerLooked;
     private SegmentScriptableObject _segment;
     private float _height;
@@ -30,6 +33,7 @@ public class Teleprompter : MonoBehaviour
         }
         _segment = segment;
         textContent.text = segment.text;
+        lightMesh.material = LightOnMaterial;
         _height = textContent.preferredHeight;
         textBox.anchoredPosition = new Vector3(-7, -(_height / 2 + textOffSet), -6);
         _active = true;
@@ -54,6 +58,7 @@ public class Teleprompter : MonoBehaviour
 
             _height = 0;
             _segment = null;
+            lightMesh.material = LightOffMaterial;
             TeleprompterManager.instance.NextSegment();
         }
     }
